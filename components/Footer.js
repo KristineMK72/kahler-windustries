2e8100952af3b9eb86a0147f81993626e2445bbe
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-24 10:11:10 -0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="p-6 text-center bg-gray-100 mt-10">
+      <p className="text-gray-600">
+        © {new Date().getFullYear()} Kahler Windustries — All Rights Reserved
+      </p>
+    </footer>
+  );
+}
