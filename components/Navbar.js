d9fd4ed1c1bc006bb169becb17f398118c6f1076
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-24 17:58:42 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="p-4 flex justify-between bg-white shadow">
-      <h1 className="font-bold text-xl">Kahler Windustries</h1>
-      <div className="space-x-6">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/services">Services</Link>
+      <Link href="/contact">Contact</Link>
     </nav>
   );
 }
